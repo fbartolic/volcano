Fixing errorbar scaling

paper/figures/irtf_ingress_egress.py
@@ -190,10 +190,7 @@ with model:
 
 # Compute residuals
 res_in = lc_in["flux"].value - soln["flux_pred_in"]
-res_in = res_in / np.std(res_in)
-
 res_eg = lc_eg["flux"].value - soln["flux_pred_eg"]
-res_eg = res_eg / np.std(res_eg)
 
 # Initialize maps
 map = starry.Map(ydeg_inf)
@@ -308,7 +305,13 @@ ax_lc[0].plot(t_dense_in, soln["flux_dense_in"], "C1-")  # Model
 
 # Residuals
 ax_res[0].errorbar(
-    t_in, res_in, f_err_in, color="black", fmt=".", ecolor="black", alpha=0.4,
+    t_in,
+    res_in / np.std(res_in),
+    f_err_in / np.std(res_in),
+    color="black",
+    fmt=".",
+    ecolor="black",
+    alpha=0.4,
 )
 
 # Plot egress
@@ -325,7 +328,13 @@ ax_lc[1].errorbar(
 ax_lc[1].plot(t_dense_eg, soln["flux_dense_eg"], "C1-")
 
 ax_res[1].errorbar(
-    t_eg, res_eg, f_err_eg, color="black", fmt=".", ecolor="black", alpha=0.4,
+    t_eg,
+    res_eg / np.std(res_eg),
+    f_err_eg / np.std(res_eg),
+    color="black",
+    fmt=".",
+    ecolor="black",
+    alpha=0.4,
 )
 
 

paper/figures/pixels_vs_harmonics.py
@@ -230,10 +230,11 @@ def plot_everything(
     ax_lc.set_xticklabels([])
 
     # Residuals
+    res_norm = np.std(res_pix_exp)  # normalize
     ax_res.errorbar(
         xo_sim,
-        residuals,
-        f_err,
+        residuals / res_norm,
+        f_err / res_norm,
         color="black",
         fmt=".",
         ecolor="black",
@@ -262,16 +263,10 @@ flux_dense_ylm = soln_ylm["flux_dense"] / norm
 flux_dense_pix_gauss = soln_pix_gauss["flux_dense"] / norm
 flux_dense_pix_exp = soln_pix_exp["flux_dense"] / norm
 
-# Compute residuals
+# Compute  residuals
 res_ylm = f_obs - MAP_obs_ylm
-res_ylm = res_ylm / np.std(res_ylm)
-
 res_pix_gauss = f_obs - MAP_obs_pix_gauss
-res_pix_gauss = res_pix_gauss / np.std(res_pix_gauss)
-
 res_pix_exp = f_obs - MAP_obs_pix_exp
-res_pix_exp = res_pix_exp / np.std(res_pix_exp)
-
 
 # Set up the plot
 nim = 8

paper/figures/simulated_ingress_egress.py
@@ -143,9 +143,7 @@ flux_obs_eg = soln["flux_pred_eg"] / norm
 
 # Compute residuals
 res_in = f_obs_in - flux_obs_in
-res_in = res_in / np.std(res_in)
 res_eg = f_obs_eg - flux_obs_eg
-res_eg = res_eg / np.std(res_eg)
 
 
 def lon_lat_to_mollweide(lon, lat):
@@ -265,7 +263,13 @@ ax_lc[0].plot(xo_in_dense, flux_in_dense, "C1-")  # Model
 
 # Residuals
 ax_res[0].errorbar(
-    xo_in, res_in, f_err_in, color="black", fmt=".", ecolor="black", alpha=0.4,
+    xo_in,
+    res_in / np.std(res_in),
+    f_err_in / np.std(res_in),
+    color="black",
+    fmt=".",
+    ecolor="black",
+    alpha=0.4,
 )
 
 # Plot egress
@@ -282,7 +286,13 @@ ax_lc[1].errorbar(
 ax_lc[1].plot(xo_eg_dense, flux_eg_dense, "C1-")
 
 ax_res[1].errorbar(
-    xo_eg, res_eg, f_err_eg, color="black", fmt=".", ecolor="black", alpha=0.4,
+    xo_eg,
+    res_eg / np.std(res_eg),
+    f_err_eg / np.std(res_eg),
+    color="black",
+    fmt=".",
+    ecolor="black",
+    alpha=0.4,
 )
 
 # Make broken axis